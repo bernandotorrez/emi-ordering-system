d7840401cc394eb42d0342269c10299c655826a3
Bernando Torrez <bernandotorrez4@gmail.com>
2020-12-03 00:23:39 +0700

    fix bug

--- app/Http/Livewire/Page/Home/HomeIndex.php
+++ app/Http/Livewire/Page/Home/HomeIndex.php
@@ -2,17 +2,14 @@
 
 namespace App\Http\Livewire\Page\Home;
 
-use App\Repository\Eloquent\MenuUserGroupRepository;
 use Livewire\Component;
 
 class HomeIndex extends Component
 {
     protected $pageTitle = "Home";
     
-    public function render(MenuUserGroupRepository $menuUserGroupRepository)
+    public function render()
     {
-        $dataParentMenu = $menuUserGroupRepository->getDynamicMenu(2);
-        dd($dataParentMenu);
         return view('livewire.page.home.home-index')->layout('layouts.app', array('title' => $this->pageTitle));
     }
 }
