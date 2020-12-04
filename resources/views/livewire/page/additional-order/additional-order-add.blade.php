c2c7a2f1ab3e046824263c59a24527be94ed7e68
Bernando Torrez <bernandotorrez4@gmail.com>
2020-12-04 18:11:12 +0700

    additonal order add on progress

--- resources/views/livewire/page/additional-order/additional-order-add.blade.php
+++ resources/views/livewire/page/additional-order/additional-order-add.blade.php
@@ -8,25 +8,66 @@
                 {!! session('action_message') !!}
                 @endif
 
-                <div class="row mb-4">
-                    <div class="col-xl-12 col-md-12 col-sm-12 col-12">
-                        <h4>{{ $pageTitle }}</h4>
-                    </div>
-                </div>
-
-                <form>
-                    <div class="form-row mb-4">
-                        <div class="form-group col-md-6">
-                            <label for="inputEmail4">Email</label>
-                            <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                <form id="contact" class="section contact">
+                    <div class="info">
+                        <h5 class="mb-4" >{{ $pageTitle }}</h5>
+                        <div class="row">
+                            <div class="col-md-11 mx-auto">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="country">Country</label>
+                                            <select class="form-control" id="country">
+                                                <option>All Countries</option>
+                                                <option selected="">United States</option>
+                                                <option>India</option>
+                                                <option>Japan</option>
+                                                <option>China</option>
+                                                <option>Brazil</option>
+                                                <option>Norway</option>
+                                                <option>Canada</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="address">Address</label>
+                                            <input type="text" class="form-control mb-4" id="address"
+                                                placeholder="Address" value="New York">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="location">Location</label>
+                                            <input type="text" class="form-control mb-4" id="location"
+                                                placeholder="Location">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="phone">Phone</label>
+                                            <input type="text" class="form-control mb-4" id="phone"
+                                                placeholder="Write your phone number here" value="+1 (530) 555-12121">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="email">Email</label>
+                                            <input type="text" class="form-control mb-4" id="email"
+                                                placeholder="Write your email here" value="Jimmy@gmail.com">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="website1">Website</label>
+                                            <input type="text" class="form-control mb-4" id="website1"
+                                                placeholder="Write your website here">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="form-group col-md-6">
-                            <label for="inputPassword4">Password</label>
-                            <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
-                        </div>
                     </div>
-                    
-                    <button type="submit" class="btn btn-primary mt-3">Sign in</button>
                 </form>
 
             </div>
